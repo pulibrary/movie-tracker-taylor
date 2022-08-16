0c8b9aff80a14ee7035456978e420c7be9c34763
Taylor Yamashita <taylory@princeton.edu>
2022-08-16 11:16:56 -0700

    set up rspec tests for watchlists and items

---
+++ spec/models/watchlist_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Watchlist, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject(:watchlist) { Watchlist.new }
+
+  describe "#" do
+    it "gets index" do
+    end
+
+    it "gets new" do
+    end
+
+    it "creates watchlist" do 
+    end
+
+    it "shows watchlist" do 
+    end
+
+    it "edits watchlist" do 
+    end
+
+    it "updates watchlist" do 
+    end
+
+    it "destroys watchlist" do
+    end
+  end
+end
