f34730f07063fa8c4a9279085c685b6c9557069a
Taylor Yamashita <taylory@princeton.edu>
2022-08-17 10:57:33 -0700

    add index and show request specs

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,4 +1,21 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe "Items", type: :request do
+  before(:all) do
+    @item = Item.new(title: "title")
+  end
+  
+  describe "GET /index" do
+    it "returns http success" do
+      get "/watchlists/1#index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/watchlists/1#show"
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
