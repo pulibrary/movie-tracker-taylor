60b09176481165faa19781291ba7568c2da12aab
Taylor Yamashita <taylory@princeton.edu>
2022-08-17 10:16:48 -0700

    minor formatting

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Item, type: :model do
-  let(:watchlist) { Watchlist.new(list_name: "test name") }
-  subject(:item) { Item.new(watchlist: watchlist, title: "test title") }
+  let(:watchlist) { Watchlist.new(list_name: "list name") }
+  subject(:item) { Item.new(watchlist: watchlist, title: "title") }
 
   it "is valid with valid attributes" do
     expect(item).to be_valid
