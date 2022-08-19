fd85e69d4c8a7c03e1427392ffeb69cf65572099
Taylor Yamashita <taylory@princeton.edu>
2022-08-19 11:35:07 -0700

    validate presence of user email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :watchlists, dependent: :destroy
+  validates_presence_of :email, :password
 
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
