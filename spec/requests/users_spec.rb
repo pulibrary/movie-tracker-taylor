505203624b5541c2c5cb91129e7158de8d2425f3
Taylor Yamashita <taylory@princeton.edu>
2022-08-19 10:20:15 -0700

    use helpers to create and log in user before running request specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "Users", type: :request do
+  let(:user) { User.create(email: "test@gmail.com") }
+  
+  before do
+    sign_in(user)
+  end
+
   describe "GET /new" do
     it "returns http success" do
       get "/users/new"
