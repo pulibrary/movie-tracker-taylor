afae4535fe0971b1af2eaf64e1fe2441e89a7fcd
Taylor Yamashita <taylory@princeton.edu>
2022-08-19 10:03:18 -0700

    more requirements and reformatting

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,10 @@
-require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
-
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require "spec_helper"
 require "rspec/rails"
+require "devise"
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -12,17 +13,18 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
-Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join("spec", "support", "**", "*.rb")].sort.each { |file| require file }
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  
   config.use_transactional_fixtures = true
-  config.infer_spec_type_from_file_location!
-  config.filter_rails_from_backtrace!
 
   config.before(:suite) do
     DatabaseCleaner.start
   ensure
     DatabaseCleaner.clean
   end
+  
+  config.infer_spec_type_from_file_location!
 end
